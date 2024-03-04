b78d827a83355580e966ba42e3a2d9873ae7f1a2
“RDHSAHS” <rdwikaharris@gmail.com>
2024-03-04 09:56:31 +0700

    feat: Add seeders_data: tvitm

---
+++ lib/core/database/seeders_data/tvitm.dart
@@ -0,0 +1,122 @@
+const tvitm = [
+  {
+    "docid": "cd3ec063-7831-44d9-bd26-be01c54407ee",
+    "createdate": "2023-10-12T04:37:43.000Z",
+    "updatedate": "2023-10-12T04:45:13.000Z",
+    "tsitm_id": "29e3ee57-e242-4e38-a511-4a3e5d93fe7b",
+    "toven_id": "9883b75f-714d-4709-8cc5-f4fe633fab62",
+    "listing": 1,
+    "minorder": 1,
+    "multipyorder": 1,
+    "canorder": 1,
+    "dflt": 1
+  },
+  {
+    "docid": "7f26ea84-4486-4c7d-b884-66b8bcf883f0",
+    "createdate": "2023-10-12T04:37:43.000Z",
+    "updatedate": "2023-10-12T04:45:14.000Z",
+    "tsitm_id": "36a989a7-8986-4c34-b0b5-e52b527d1065",
+    "toven_id": "9883b75f-714d-4709-8cc5-f4fe633fab62",
+    "listing": 1,
+    "minorder": 1,
+    "multipyorder": 1,
+    "canorder": 1,
+    "dflt": 1
+  },
+  {
+    "docid": "3f344c60-1cee-4c02-8140-fad18c81c40a",
+    "createdate": "2023-10-12T04:37:43.000Z",
+    "updatedate": "2023-10-12T04:45:14.000Z",
+    "tsitm_id": "c5aaf68d-2477-43f5-aa7a-66b94ccaa345",
+    "toven_id": "9883b75f-714d-4709-8cc5-f4fe633fab62",
+    "listing": 1,
+    "minorder": 1,
+    "multipyorder": 1,
+    "canorder": 1,
+    "dflt": 1
+  },
+  {
+    "docid": "d2bc71f9-f3e4-44db-b021-53508d330744",
+    "createdate": "2023-10-12T04:37:43.000Z",
+    "updatedate": "2023-10-12T04:45:14.000Z",
+    "tsitm_id": "3042a485-ca9e-4151-9931-2fc1f83b56ba",
+    "toven_id": "9883b75f-714d-4709-8cc5-f4fe633fab62",
+    "listing": 1,
+    "minorder": 1,
+    "multipyorder": 1,
+    "canorder": 1,
+    "dflt": 1
+  },
+  {
+    "docid": "18af1a96-d18d-4972-9cac-bcc26c510eb8",
+    "createdate": "2023-10-12T04:37:43.000Z",
+    "updatedate": "2023-10-12T04:45:14.000Z",
+    "tsitm_id": "5ae84d99-f1bf-400d-8a54-84fa6a95f7af",
+    "toven_id": "9883b75f-714d-4709-8cc5-f4fe633fab62",
+    "listing": 1,
+    "minorder": 1,
+    "multipyorder": 1,
+    "canorder": 1,
+    "dflt": 1
+  },
+  {
+    "docid": "a92d547a-7ba0-4c35-bde2-97c1f008bd18",
+    "createdate": "2023-10-12T04:37:43.000Z",
+    "updatedate": "2023-10-12T04:45:14.000Z",
+    "tsitm_id": "e0abb042-b107-47a5-a1a1-c518acbfc857",
+    "toven_id": "9883b75f-714d-4709-8cc5-f4fe633fab62",
+    "listing": 1,
+    "minorder": 1,
+    "multipyorder": 1,
+    "canorder": 1,
+    "dflt": 1
+  },
+  {
+    "docid": "9a54a94e-c7c4-4643-98d8-02c009604ac4",
+    "createdate": "2023-10-12T04:37:44.000Z",
+    "updatedate": "2023-10-12T04:45:14.000Z",
+    "tsitm_id": "de9a6f5d-f7f3-4fb9-8017-cc59604a6df4",
+    "toven_id": "9883b75f-714d-4709-8cc5-f4fe633fab62",
+    "listing": 1,
+    "minorder": 1,
+    "multipyorder": 1,
+    "canorder": 1,
+    "dflt": 1
+  },
+  {
+    "docid": "631f7dca-cb88-4b34-999b-9821cf2aad90",
+    "createdate": "2023-10-25T19:57:39.000Z",
+    "updatedate": "2023-11-03T09:53:27.000Z",
+    "tsitm_id": "a24ab6a7-222f-425d-b858-4a3f23990fe2",
+    "toven_id": "870d0121-2c51-4b95-9da3-4677a3e85268",
+    "listing": 1,
+    "minorder": 1,
+    "multipyorder": 1,
+    "canorder": 1,
+    "dflt": 1
+  },
+  {
+    "docid": "0a8bf73b-09d0-4dc6-96a8-c17ab7484a22",
+    "createdate": "2023-10-25T19:57:39.000Z",
+    "updatedate": "2023-11-03T09:53:27.000Z",
+    "tsitm_id": "94d88661-e3e6-4969-bacf-c4c08b348b24",
+    "toven_id": "870d0121-2c51-4b95-9da3-4677a3e85268",
+    "listing": 1,
+    "minorder": 1,
+    "multipyorder": 1,
+    "canorder": 1,
+    "dflt": 1
+  },
+  {
+    "docid": "a33d8687-6a92-4def-a0e9-227dfb0a121b",
+    "createdate": "2023-10-25T20:15:34.000Z",
+    "updatedate": "2023-11-03T09:53:27.000Z",
+    "tsitm_id": "03c9c488-c51e-4180-9d7b-198b265c2374",
+    "toven_id": "870d0121-2c51-4b95-9da3-4677a3e85268",
+    "listing": 1,
+    "minorder": 1,
+    "multipyorder": 1,
+    "canorder": 1,
+    "dflt": 1
+  }
+];
