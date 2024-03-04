0b6828ffa9b3384cf7795daa9d254dee90773994
“RDHSAHS” <rdwikaharris@gmail.com>
2024-03-04 09:01:11 +0700

    feat: Add seeders_data: topmt

---
+++ lib/core/database/seeders_data/topmt.dart
@@ -0,0 +1,44 @@
+const topmt = [
+  {
+    "docid": "33d3eefe-7fa0-40b5-a208-2f235a09c71a",
+    "createdate": "2023-08-28 04:08:00",
+    "updatedate": "2023-08-28 04:08:00",
+    "paytypecode": "1",
+    "description": "Tunai"
+  },
+  {
+    "docid": "226168e0-62f5-475f-9e5e-0f708dfd8e2d",
+    "createdate": "2023-08-28 04:08:00",
+    "updatedate": "2023-08-28 04:08:00",
+    "paytypecode": "2",
+    "description": "Debit"
+  },
+  {
+    "docid": "f6cea872-8bdd-4240-a135-9b299f01a6df",
+    "createdate": "2023-08-28 04:08:00",
+    "updatedate": "2023-08-28 04:08:00",
+    "paytypecode": "3",
+    "description": "Kredit"
+  },
+  {
+    "docid": "20ab10c1-1f7b-4362-b400-a3bef4a13c84",
+    "createdate": "2023-08-28 04:08:00",
+    "updatedate": "2023-08-28 04:08:00",
+    "paytypecode": "4",
+    "description": "Lainnya"
+  },
+  {
+    "docid": "c06fb2ab-e3f0-4e64-b4c5-3033235e6746",
+    "createdate": "2023-08-28 04:08:00",
+    "updatedate": "2023-08-28 04:08:00",
+    "paytypecode": "5",
+    "description": "eWallet"
+  },
+  {
+    "docid": "fd76f514-e4d3-4338-bee3-be5a4e2d58de",
+    "createdate": "2023-08-28 04:08:00",
+    "updatedate": "2023-08-28 04:08:00",
+    "paytypecode": "6",
+    "description": "Voucher"
+  }
+];
