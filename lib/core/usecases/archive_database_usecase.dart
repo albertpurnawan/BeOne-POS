833bcc47d2b91f42bbd692bddabc2ce6b216fe17
“RDHSAHS” <rdwikaharris@gmail.com>
2024-08-07 14:19:37 +0700

    chore: archive database usecase

--- lib/core/usecases/archive_database_usecase.dart
+++ lib/core/usecases/archive_database_usecase.dart
@@ -1,15 +1,25 @@
-// import 'package:flutter/material.dart';
-// import 'package:get_it/get_it.dart';
-// import 'package:pos_fe/core/usecases/usecase.dart';
-// import 'package:pos_fe/features/settings/domain/usecases/decrypt.dart';
+import 'dart:developer';
 
-// class ArchiveDatabaseParams {
-//   final BuildContext context;
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:pos_fe/core/usecases/usecase.dart';
+import 'package:pos_fe/features/settings/domain/usecases/decrypt.dart';
 
-//   ArchiveDatabaseParams({required this.context});
-// }
+class ArchiveDatabaseParams {
+  final BuildContext context;
 
-// class ArchiveDatabaseUseCase implements UseCase<void, ArchiveDatabaseParams> {
-//   final _databaseName = "pos_fe.db";
-//   final decryptPasswordUseCase = GetIt.instance<DecryptPasswordUseCase>();
-// }
+  ArchiveDatabaseParams({required this.context});
+}
+
+class ArchiveDatabaseUseCase implements UseCase<void, ArchiveDatabaseParams> {
+  final _databaseName = "pos_fe.db";
+  final decryptPasswordUseCase = GetIt.instance<DecryptPasswordUseCase>();
+
+  @override
+  Future<void> call({ArchiveDatabaseParams? params}) async {
+    final context = params!.context;
+    try {} catch (e) {
+      log("Error archiving database: $e");
+    }
+  }
+}
