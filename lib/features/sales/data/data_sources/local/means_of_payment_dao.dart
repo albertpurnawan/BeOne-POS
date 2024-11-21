2642a57055c366054bcd4ac4a9f97efec8836fe8
“RDHSAHS” <rdwikaharris@gmail.com>
2024-11-21 19:11:24 +0700

    fix: mop debit/kredit to edc)

--- lib/features/sales/data/data_sources/local/means_of_payment_dao.dart
+++ lib/features/sales/data/data_sources/local/means_of_payment_dao.dart
@@ -12,8 +12,7 @@ class MeansOfPaymentDao extends BaseDao<MeansOfPaymentModel> {
         );
 
   @override
-  Future<MeansOfPaymentModel?> readByDocId(
-      String docId, Transaction? txn) async {
+  Future<MeansOfPaymentModel?> readByDocId(String docId, Transaction? txn) async {
     DatabaseExecutor dbExecutor = txn ?? db;
     final res = await dbExecutor.query(
       tableName,
@@ -30,13 +29,10 @@ class MeansOfPaymentDao extends BaseDao<MeansOfPaymentModel> {
     DatabaseExecutor dbExecutor = txn ?? db;
     final result = await dbExecutor.query(tableName);
 
-    return result
-        .map((itemData) => MeansOfPaymentModel.fromMap(itemData))
-        .toList();
+    return result.map((itemData) => MeansOfPaymentModel.fromMap(itemData)).toList();
   }
 
-  Future<MeansOfPaymentModel?> readByDescription(
-      String description, Transaction? txn) async {
+  Future<MeansOfPaymentModel?> readByDescription(String description, Transaction? txn) async {
     DatabaseExecutor dbExecutor = txn ?? db;
     final res = await dbExecutor.query(
       tableName,
@@ -48,26 +44,25 @@ class MeansOfPaymentDao extends BaseDao<MeansOfPaymentModel> {
     return res.isNotEmpty ? MeansOfPaymentModel.fromMap(res[0]) : null;
   }
 
-  Future<List<MopSelectionModel>> readByPaytypeCode(
-      String payTypeCode, String edc) async {
+  Future<List<MopSelectionModel>> readByPaytypeCode(String payTypeCode, String edc) async {
     final result = await db.rawQuery('''
       SELECT p3.docid AS tpmt3Id, p3.tpmt1Id, p1.mopalias, p1.bankcharge, p.paytypecode, 
-        p.description, p1.subtype,p4.docid AS tpmt4Id, p4.description AS edcdesc
-      FROM tpmt3 AS p3
-      INNER JOIN (
-          SELECT mopalias, bankcharge, docid, topmtId, subtype 
-          FROM tpmt1
-      ) AS p1 ON p3.tpmt1Id = p1.docid
-      INNER JOIN (
-          SELECT paytypecode, description, docid 
-          FROM topmt
-          WHERE paytypecode = ? 
-      ) AS p ON p1.topmtId = p.docid
-      LEFT JOIN (
-          SELECT docid, description 
-          FROM tpmt4
-      ) AS p4 ON p1.mopalias LIKE p4.description || '%'
-      WHERE p4.description = ?
+              p.description, p1.subtype,p4.docid AS tpmt4Id, p4.description AS edcdesc
+            FROM tpmt3 AS p3
+            INNER JOIN (
+                SELECT mopalias, bankcharge, docid, topmtId, subtype, tpmt4Id
+                FROM tpmt1
+            ) AS p1 ON p3.tpmt1Id = p1.docid
+            INNER JOIN (
+                SELECT paytypecode, description, docid 
+                FROM topmt
+                WHERE paytypecode = ? 
+            ) AS p ON p1.topmtId = p.docid
+            LEFT JOIN (
+                SELECT docid, description 
+                FROM tpmt4
+            ) AS p4 ON p1.tpmt4Id = p4.docid
+            WHERE p4.docid = ?
     ''', [payTypeCode, edc]);
 
     return result.map((e) => MopSelectionModel.fromMap(e)).toList();
